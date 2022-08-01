d8785f9a9758fb7147c59e2a483a21c893151ba0
Taras <81424893+tar1kkk@users.noreply.github.com>
2022-08-01 18:23:24 +0300

    UI directory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import MyInput from "./components/UI/input/MyInput";
+import MyButton from "./components/UI/button/MyButton"
+import { useState } from 'react';
 
 function App() {
+  const [purchase, setPurchase] = useState([]);
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+
+  function addNewPurchase() {
+    const newPurchase = {
+      title,
+      price,
+      id: Date.now()
+    }
+    setPurchase([newPurchase, ...purchase])
+  }
+
   return (
-    <div className="App">
-      Hello
-    </div>
+    <div className="App" >
+
+      <MyInput value={title} onChange={e => setTitle(e.target.value)} type='text' placeholder='Purchase' />
+      <MyInput value={price} onChange={e => setPrice(e.target.value)} type='number' placeholder="Price" />
+      <MyButton onClick={() => { addNewPurchase() }} >Создать пост</MyButton>
+
+    </div >
   );
 }
 
